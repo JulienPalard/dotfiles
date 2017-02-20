80462259c7b3945a942552b1e6b06f24ac5f7b2c
Julien Palard <julien@palard.fr>
2017-02-20 14:13:54 +0100

    Absolute copy.py path in install.sh.

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 rm -fr /tmp/.dotfiles/
 git clone -q git@github.com:JulienPalard/dotfiles.git /tmp/.dotfiles/ &&
-    ./copy.py /tmp/.dotfiles/ ~/ --exclude README.md .git copy.py install.sh &&
+    /tmp/.dotfiles/copy.py /tmp/.dotfiles/ ~/ --exclude README.md .git copy.py install.sh &&
     rm -fr /tmp/.dotfiles/
