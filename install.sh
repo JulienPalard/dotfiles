45405769e8bf103f0fe48a0d699552b56ff96c40
Julien Palard <julien@palard.fr>
2017-02-20 14:12:35 +0100

    FIX: copy.py name in install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 rm -fr /tmp/.dotfiles/
 git clone -q git@github.com:JulienPalard/dotfiles.git /tmp/.dotfiles/ &&
-    ./copy_to_home.py /tmp/.dotfiles/ ~/ --exclude README.md .git copy.py install.sh &&
+    ./copy.py /tmp/.dotfiles/ ~/ --exclude README.md .git copy.py install.sh &&
     rm -fr /tmp/.dotfiles/
