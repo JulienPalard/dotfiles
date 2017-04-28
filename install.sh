a0e846ec167f169540ca28bfac25b2de702cfe07
Julien Palard <julien@palard.fr>
2017-04-28 11:26:18 +0200

    Use readonly clone to avoid asking for the passphrase.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -fr /tmp/.dotfiles/
-git clone -q git@github.com:JulienPalard/dotfiles.git /tmp/.dotfiles/ &&
+git clone -q https://github.com/JulienPalard/dotfiles.git /tmp/.dotfiles/ &&
     /tmp/.dotfiles/interactive_copy.py /tmp/.dotfiles/ ~/ --exclude README.md .git interactive_copy.py install.sh __pycache__ &&
     rm -fr /tmp/.dotfiles/
