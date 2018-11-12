aabe0dcccbdd7106b286408148582b5de29d7ac1
Julien Palard <julien@palard.fr>
2018-11-12 09:30:11 +0100

    Reuse fonts in Xresources when changing size.

--- .config/pythonstartup.py
+++ .config/pythonstartup.py
@@ -1,15 +1,22 @@
+import os
+
+with open(os.path.expanduser("~/.Xresources")) as xresources_file:
+    for line in xresources_file:
+        if line.startswith("URxvt.font"):
+            font = line.split(":", 1)[1].strip()
+
 class FontSize:
     def __init__(self):
         self.font_size = 20
 
     def __iadd__(self, x):
         self.font_size += x
-        print(f"\33]50;xft:LiberationMono:size={self.font_size}\007")
+        print(f"\33]710;xft:LiberationMono:size={self.font_size},{font}\007")
         return self
 
     def __isub__(self, x):
         self.font_size -= x
-        print(f"\33]50;xft:LiberationMono:size={self.font_size}\007")
+        print(f"\33]710;xft:LiberationMono:size={self.font_size},{font}\007")
         return self
 
 
