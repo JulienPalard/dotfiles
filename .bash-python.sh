9bf7f2b5e0e25c72ba5a4ac94b1011e20b72d3c1
Julien Palard <julien@palard.fr>
2021-11-06 10:25:14 +0100

    Bump Python versions.

--- .bash-python.sh
+++ .bash-python.sh
@@ -45,9 +45,9 @@ compile_all_pythons()
     compile_python 3.6.15 &
     compile_python 3.7.12 &
     compile_python 3.8.12 &
-    compile_python 3.9.7 &
+    compile_python 3.9.8 &
     compile_python 3.10.0 &
-    compile_python 3.11.0 a1
+    compile_python 3.11.0 a2 &
     wait
 }
 
